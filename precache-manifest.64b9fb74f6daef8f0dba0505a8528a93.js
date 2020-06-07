212f46a8f5dd7db2868c5292bf0dfbc734a0d4e9
juan camilo acevedo <juancaacevedo10@hotmail.com>
2020-06-07 09:28:28 -0500

    Updates

---
+++ precache-manifest.64b9fb74f6daef8f0dba0505a8528a93.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5db127c43224a8406a96ae0fd4abcc1d",
+    "url": "/Pomodoro_Clock/index.html"
+  },
+  {
+    "revision": "885abcd4cdd7afaebe86",
+    "url": "/Pomodoro_Clock/static/css/2.02b1f3e9.chunk.css"
+  },
+  {
+    "revision": "e578644b47940731c310",
+    "url": "/Pomodoro_Clock/static/css/main.26544838.chunk.css"
+  },
+  {
+    "revision": "885abcd4cdd7afaebe86",
+    "url": "/Pomodoro_Clock/static/js/2.46f5008d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pomodoro_Clock/static/js/2.46f5008d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e578644b47940731c310",
+    "url": "/Pomodoro_Clock/static/js/main.144f6f19.chunk.js"
+  },
+  {
+    "revision": "6140a6fec456746e3801",
+    "url": "/Pomodoro_Clock/static/js/runtime-main.038994bf.js"
+  }
+]);
